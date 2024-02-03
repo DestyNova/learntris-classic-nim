4b1609a9da57197cfc4127d47c8f49a41b21e388
Oisín Mac Fhearaí <denpashogai@gmail.com>
2024-02-03 01:18:57 +0000

    Get rid of terminal import since getch() apparently isn't needed after all

--- tetris.nim
+++ tetris.nim
@@ -1,20 +1,25 @@
-import terminal
-
 const
   W = 10
   H = 22
 
-type Grid = array[H, array[W, bool]]
-
+type Grid = array[H, array[W, char]]
 var g: Grid
+for r in 0..<H:
+  for c in 0..<W:
+    g[r][c] = '.'
 
 while true:
-  let c = getch()
+  let c = stdin.readLine[0]
   case c:
     of 'q': break
-    of 'p':
+    of 'p': # print
       for r in 0..<H:
         for c in 0..<W:
-          stdout.write(if g[r][c]: "* " else: ". ")
+          stdout.write(g[r][c] & " ")
         echo ""
+    of 'g': # given
+      for r in 0..<H:
+        let line = stdin.readLine
+        for c in 0..<W:
+          g[r][c] = line[c*2]
     else: continue
