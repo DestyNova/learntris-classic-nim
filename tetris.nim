0d551d6f89d4b659d837f4a229349c5117e09d43
Oisín Mac Fhearaí <denpashogai@gmail.com>
2024-02-03 01:24:42 +0000

    Add io.score.s

--- tetris.nim
+++ tetris.nim
@@ -3,7 +3,10 @@ const
   H = 22
 
 type Grid = array[H, array[W, char]]
-var g: Grid
+
+var
+  g: Grid
+  score = 0
 
 proc clear(g: var Grid) =
   for r in 0..<H:
@@ -13,18 +16,19 @@ proc clear(g: var Grid) =
 g.clear
 
 while true:
-  let c = stdin.readLine[0]
+  let c = stdin.readLine
   case c:
-    of 'q': break
-    of 'p': # print
+    of "q": break
+    of "p": # print
       for r in 0..<H:
         for c in 0..<W:
           stdout.write(g[r][c] & " ")
         echo ""
-    of 'g': # given
+    of "g": # given
       for r in 0..<H:
         let line = stdin.readLine
         for c in 0..<W:
           g[r][c] = line[c*2]
-    of 'c': g.clear
+    of "c": g.clear
+    of "?s": echo score
     else: continue
