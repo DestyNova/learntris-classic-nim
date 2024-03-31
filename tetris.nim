4d89822006faa0ce0e564579c695027833a41f05
Oisín Mac Fhearaí <denpashogai@gmail.com>
2024-03-31 04:04:23 +0100

    Pieces.O

--- tetris.nim
+++ tetris.nim
@@ -5,11 +5,11 @@ const
   H = 22
 
 type Grid = seq[seq[char]]
-type Pieces = enum I
+type Pieces = enum I, O
 
 var
   g: Grid = newSeqWith(H, newSeq[char](W))
-  activeRegion: Grid = newSeqWith(4, newSeq[char](4))
+  activeRegion: Grid
   score = 0
   cleared = 0
 
@@ -31,15 +31,28 @@ proc step() =
       cleared += 1
       for c in 0..<W: g[r][c] = '.'
 
-proc setPiece(p: Pieces) =
-  # clear the active region
-  for r in 0..<4:
-    for c in 0..<4:
+proc makeActiveRegion(p: Pieces) =
+  let (w,h) = case p:
+    of Pieces.I: (4,4)
+    of Pieces.O: (2,2)
+
+  activeRegion = newSeqWith(h, newSeq[char](w))
+
+  for r in 0..<h:
+    for c in 0..<w:
       activeRegion[r][c] = '.'
+
+proc setPiece(p: Pieces) =
+  makeActiveRegion(p)
 
   if p == Pieces.I:
     for c in 0..<4:
       activeRegion[1][c] = 'c'
+
+  elif p == Pieces.O:
+    for c in 0..<2:
+      activeRegion[0][c] = 'y'
+      activeRegion[1][c] = 'y'
 
 proc showGrid(grid: Grid) =
   for r in 0..<grid.len:
@@ -62,5 +75,6 @@ while true:
     of "?n": echo cleared
     of "s": step()
     of "I": setPiece(Pieces.I)
+    of "O": setPiece(Pieces.O)
     of "t": showGrid(activeRegion)
     else: continue
