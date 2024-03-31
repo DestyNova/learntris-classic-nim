dbcfd3db747c32842e09ef11dcab445d2b2ed337
Oisín Mac Fhearaí <denpashogai@gmail.com>
2024-03-31 04:24:21 +0100

    Pieces.L

--- tetris.nim
+++ tetris.nim
@@ -5,7 +5,7 @@ const
   H = 22
 
 type Grid = seq[seq[char]]
-type Pieces = enum I, O, Z, S, J
+type Pieces = enum I, O, Z, S, J, L
 
 var
   g: Grid = newSeqWith(H, newSeq[char](W))
@@ -35,7 +35,7 @@ proc makeActiveRegion(p: Pieces) =
   let (w,h) = case p:
     of Pieces.I: (4,4)
     of Pieces.O: (2,2)
-    of Pieces.Z, Pieces.S, Pieces.J: (3,3)
+    of Pieces.Z, Pieces.S, Pieces.J, Pieces.L: (3,3)
 
   activeRegion = newSeqWith(h, newSeq[char](w))
 
@@ -73,6 +73,12 @@ proc setPiece(p: Pieces) =
     activeRegion[1][1] = 'b'
     activeRegion[1][2] = 'b'
 
+  elif p == Pieces.L:
+    activeRegion[0][2] = 'o'
+    activeRegion[1][0] = 'o'
+    activeRegion[1][1] = 'o'
+    activeRegion[1][2] = 'o'
+
 proc showGrid(grid: Grid) =
   for r in 0..<grid.len:
     for c in 0..<grid[0].len:
@@ -99,5 +105,6 @@ while not stdin.endOfFile:
       of "Z": setPiece(Pieces.Z)
       of "S": setPiece(Pieces.S)
       of "J": setPiece(Pieces.J)
+      of "L": setPiece(Pieces.L)
       of "t": showGrid(activeRegion)
       else: continue
